929c158dc586133955e94c84fb41431c33024ec6
Sofia Riekkola <sofia.riekkola@gmail.com>
2019-11-26 14:53:27 +0200

    Update working hours

--- findapairproject/findthepair/src/main/java/com/mycompany/findthepair/Main.java
+++ findapairproject/findthepair/src/main/java/com/mycompany/findthepair/Main.java
@@ -4,7 +4,7 @@ package com.mycompany.findthepair;
 public class Main {
     
     public static void main(String[] args) {
-        Gameboard gameboard = new Gameboard(4, 5);
+        Gameboard gameboard = new Gameboard(3, 4);
         gameboard.render();
     }
     
--- findapairproject/workinghours.md
+++ findapairproject/workinghours.md
@@ -3,3 +3,4 @@
 | Date        | Hours           | Task  |
 | :-------------: |:-------------:| :-----:|
 | 20.11      | 2 | Documentation |
+| 24.11 | 3 | Base code |
